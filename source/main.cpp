bf76b0fd48c4f6d036d41b9adf85706d1101b1a1
Colben Matthew Kharrl <colben@Colbens-MacBook-Pro.local>
2017-02-23 21:54:11 -0700

    Added randomize to sort library

--- source/main.cpp
+++ source/main.cpp
@@ -10,24 +10,12 @@
 //	used for testing and debugging
 using namespace std;
 
-void Randomize(int *I, int length)
-{
-    srand(time(0));
-    int element;
-    for (int i = 0; i < length; i++) // fill array with random numbers
-    {
-		element = rand() % 100 + 1; //element E[1,100]
-		I[i] = element;
-    }
-    PrintList(I, 0, length - 1);
-}
-
 int main()
 {
-    int length = 20, x;
+    int length = 20;
     int *A = new int[length];
-	Randomize(A, length);
+	FillReverse(A, length);
 	cout << "Min-Heap: " << endl;
 	MinHeap heap (A, length);
-    PrintList(A, 0, length-1);
+    Print(A, 0, length-1);
 }
--- source/sort.cpp
+++ source/sort.cpp
@@ -9,12 +9,32 @@
 using namespace std;
 
 //	print dynamic array from index start to end
-void PrintList(int *A, int p, int r) {
+void Print(int *A, int p, int r) {
   int i;
   for (i = p; i <= r; i++) {
     cout << A[i] << " ";
   }
   cout << endl;
+}
+void Randomize(int *A, int length)
+{
+    srand(time(0));
+    int element;
+    for (int i = 0; i < length; i++) // fill array with random numbers
+    {
+		element = rand() % 100 + 1; //element E[1,100]
+		A[i] = element;
+    }
+    Print(A, 0, length - 1);
+}
+void FillReverse(int *A, int length) {
+	int j = length;
+	for (int i = 0; i < length; i++) // fill array with reverse sorted numbers
+    {
+		A[i] = j;
+		j--;
+    }
+	Print(A, 0, length - 1);
 }
 void InsertionSort(int *A, int length) {
   int key, i, j;
